d4c7cc82cc36277ec8a8124cc026c558b7c765b6
Juily <juilys23@gmail.com>
2024-06-29 23:27:17 +0530

    first commit

---
+++ src/test/java/APITest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+public class APITest {
+	@Test	
+	public void apiTest1() {
+		System.out.println("API Test 1");
+	}
+		@Test	
+		public void APITest2() {
+			System.out.println("API Test 2");
+			
+		}	
+		
+		
+}
---
+++ src/test/java/SeleniumTest.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	
+@Test	
+	public void seleniumTest1() {
+		System.out.println("Seleium Test 1");
+		
+		
+	}
+@Test	
+public void seleniumTest2() {
+	System.out.println("Seleium Test 2");
+	
+	
+}
+	
+	
+	
+
+}
